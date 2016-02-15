c4ae6032c2d7951762e6a7739b84060e31b918d3
CByrd17 <cbyrd17@yahoo.com>
2016-02-15 11:27:25 -0500

    Working to get connection made to github and static analysis configured.

---
+++ PPSJ-slack/src/chapter1/Add16And23.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chapter1;
+
+/**
+ * @author CByrd17
+ *
+ */
+public class Add16And23 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
